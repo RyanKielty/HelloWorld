1bb6f6ba74db334b704dd7a8a127fc48e43fc7d3
Ryan Kielty <ryankielty@forgerocks-mbp.apollosrc.root>
2016-11-15 11:03:25 -0600

    Second Day

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args){
-        Person p = new Person();
-        p.name = "Alice";
-        p.age = 30;
-        p.isAlive = true;
+        Person.isValidName("Bob Johnson");
 
-        int one = 1;
+        System.out.println(Person.hair);
+
     }
 }
